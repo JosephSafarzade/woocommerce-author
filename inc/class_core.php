a11494b3b511d886ff2a67af785cb2b6e535606c
iman <iman.shahinnezhad68@gmail.com>
2022-09-12 09:04:47 +0430

    flag change svg to png

--- inc/class_core.php
+++ inc/class_core.php
@@ -182,7 +182,7 @@ class wooa_core
 
         $country_code =  get_post_meta($author_id , 'wooa_author_country' , true );
 
-        $country_flag = file_get_contents('https://countryflagsapi.com/svg/' . $country_code );
+        $country_flag = file_get_contents('https://countryflagsapi.com/png/' . $country_code );
 
         return sprintf ("<div class='woo-author-flag-container'>%s</div>" , $country_flag );
 
