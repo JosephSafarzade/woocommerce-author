cc1babaad6b29a74dd02a1906cfbcf1a3005d01b
iman <iman.shahinnezhad68@gmail.com>
2022-10-08 10:54:45 +0330

    author product style 1

--- inc/class_core.php
+++ inc/class_core.php
@@ -1120,17 +1120,20 @@ class wooa_core
 
             try {
 
-                $product = new WC_Product($product_id);
+                    $product = new WC_Product($product_id);
 
                 $product_data = array(
                     'title' => $product->get_title(),
+                    'url' => $product->get_permalink(),
                     'regular_price' => $product->get_regular_price(),
                     'sale_price' => $product->get_sale_price(),
                     'image_id' => $product->get_image_id(),
                     'is_sale' => $product->is_on_sale(),
                     'image' => $product->get_image(),
                     'description' => $product->get_description(),
-                    'short_description' => $product->get_short_description()
+                    'short_description' => $product->get_short_description(),
+                    'price_with_currency'=> $product->get_price_html(),
+                    'currency' => get_woocommerce_currency_symbol(),
                 );
 
                 wooa_core::generate_single_product_box_html($product_data);
@@ -1154,11 +1157,32 @@ class wooa_core
     static public function generate_single_product_box_html($product_data){
 
    ?>
-        <div>
-            <?php echo $product_data[title] ?>
-        </div>
+            <div class="wooa-product-item-style-1  <?php echo $product_data[is_sale] ? 'is-sale' : '' ?>">
 
+                    <a href="<?php echo $product_data[url] ?>" title="<?php echo $product_data[title] ?>" class="wooa-product-item-style-1__image-container">
 
+                        <?php echo $product_data[is_sale] ? '<span>Sale</span>' : '' ?>
+
+                        <?php echo $product_data[image] ?>
+
+                    </a>
+
+                    <a href="<?php echo $product_data[url] ?>" title="<?php echo $product_data[title] ?>" class="wooa-product-item-style-1__content">
+
+                       <h5 class="wooa-product-item-style-1__title"><?php echo $product_data[title] ?></h5>
+
+                        <div class="wooa-product-item-style-1__prices">
+
+                            <div class="wooa-product-item-style-1__regular-price"><?php echo $product_data[currency] . $product_data[regular_price] ?></div>
+
+                            <div class="wooa-product-item-style-1__sale-price"><?php echo $product_data[currency] . $product_data[sale_price] ?></div>
+
+                        </div>
+
+                    </a>
+
+                     <div class="wooa-product-item-style-1__add-to-cart"> <a>ADD TO CART</a></div>
+            </div>
 
 
 
--- inc/elementor-widgets/class_elementor_widget_show_author_products.php
+++ inc/elementor-widgets/class_elementor_widget_show_author_products.php
@@ -29,6 +29,17 @@ class wooa_elementor_widget_show_author_products extends \Elementor\Widget_Base
         return ['author', 'products', 'woocommerce'];
     }
 
+    public function get_style_depends() {
+
+        if( !wp_style_is( 'frontend-general', 'registered' ) ){
+
+            wp_register_style( 'frontend-general',WOOA_ASSETS_CSS_FOLDER_URL . "/frontend-general.css" , [] ,WOOA_PLUGIN_VERSION);
+
+        }
+
+        return [ 'frontend-general' ];
+    }
+
 
     protected function register_controls()
     {
@@ -62,14 +73,14 @@ class wooa_elementor_widget_show_author_products extends \Elementor\Widget_Base
             [
                 'label' => esc_html__( 'Number of Columns', WOOA_TEXT_DOMAIN ),
                 'type' => \Elementor\Controls_Manager::SELECT,
-                'default' => '3',
+                'default' => 3,
                 'options' => [
-                    '2'     => esc_html__( 'Two', WOOA_TEXT_DOMAIN ),
-                    '3'     => esc_html__( 'Three', WOOA_TEXT_DOMAIN ),
-                    '4'     => esc_html__( 'Four', WOOA_TEXT_DOMAIN ),
+                    2    => esc_html__( 'Two', WOOA_TEXT_DOMAIN ),
+                    3     => esc_html__( 'Three', WOOA_TEXT_DOMAIN ),
+                    4     => esc_html__( 'Four', WOOA_TEXT_DOMAIN ),
                 ],
                 'selectors' => [
-                    '{{WRAPPER}} .wooa-author-products-container' => 'border-radius: {{VALUE}};',
+                    '{{WRAPPER}} .wooa-author-products-container' => 'grid-template-columns: repeat({{VALUE}}, 1fr) ;',
                 ],
             ]
         );
@@ -87,6 +98,45 @@ class wooa_elementor_widget_show_author_products extends \Elementor\Widget_Base
             ]
         );
 
+        $this->add_control(
+            'title_tag',
+            [
+                'label' => esc_html__( 'Select Title Tag', WOOA_TEXT_DOMAIN ),
+                'type' => \Elementor\Controls_Manager::SELECT2,
+                'multiple' => false,
+                'options' => wooa_core::return_container_tag_list_for_elementor_widget_setting(),
+                'default' => [ 'h5' ],
+            ]
+        );
+
+
+        $this->add_control(
+            'product-button-color',
+            [
+                'label' => esc_html__( 'Product Button Color', WOOA_TEXT_DOMAIN ),
+                'type' => \Elementor\Controls_Manager::COLOR,
+                'description' => esc_html__( ' Color for ADD TO CART Button Text ', 'elementor' ),
+                'selectors' => [
+                    '{{WRAPPER}} .wooa-product-item-style-1__add-to-cart a' => 'color: {{VALUE}} ;',
+                ],
+
+            ],
+
+        );
+
+        $this->add_control(
+            'product-button-background',
+            [
+                'label' => esc_html__( 'Product Button Background Color', WOOA_TEXT_DOMAIN ),
+                'type' => \Elementor\Controls_Manager::COLOR,
+                'description' => esc_html__( 'Background Color for ADD TO CART Button ', 'elementor' ),
+                'selectors' => [
+                    '{{WRAPPER}} .wooa-product-item-style-1__add-to-cart a' => 'background-color: {{VALUE}} ;',
+                ],
+
+            ],
+
+        );
 
 
         $this->end_controls_section();
@@ -104,8 +154,9 @@ class wooa_elementor_widget_show_author_products extends \Elementor\Widget_Base
 
     protected function render()
     {
-
         $settings = $this->get_settings_for_display();
+
+        $titleTag = $settings['title_tag'];
 
         $settings['author_id'] =  apply_filters('wooa_return_author_id',$settings['author_id'] );
 
@@ -117,7 +168,7 @@ class wooa_elementor_widget_show_author_products extends \Elementor\Widget_Base
 
         } else {
 
-            printf('<div clas="wooa-author-products-container">');
+            printf('<div class="wooa-author-products-container">');
 
                 wooa_core::generate_author_products_html($products);
 
